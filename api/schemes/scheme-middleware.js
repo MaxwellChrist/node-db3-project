0ee586fec718057177cec4a08413fbb9e7f49aad
MaxwellChrist <ribthulu@gmail.com>
2022-05-18 18:51:59 -0400

    cleaned up code

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,14 +1,4 @@
 const db = require('../../data/db-config');
-const Model = require('./scheme-model');
-
-/*
-  If `scheme_id` does not exist in the database:
-
-  status 404
-  {
-    "message": "scheme with scheme_id <actual id> not found"
-  }
-*/
 
 const checkSchemeId = async (req, res, next) => {
   const schemeIdCheck = await db('schemes').where('scheme_id', req.params.scheme_id).first();
@@ -19,14 +9,6 @@ const checkSchemeId = async (req, res, next) => {
   }
 }
 
-/*
-  If `scheme_name` is missing, empty string or not a string:
-
-  status 400
-  {
-    "message": "invalid scheme_name"
-  }
-*/
 const validateScheme = (req, res, next) => {
   if (typeof req.body.scheme_name !== "string" || req.body.scheme_name === "") {
     next({ message: "invalid scheme_name", status: 400 });
@@ -36,15 +18,6 @@ const validateScheme = (req, res, next) => {
   }
 }
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
-
-  status 400
-  {
-    "message": "invalid step"
-  }
-*/
 const validateStep = (req, res, next) => {
   if (typeof req.body.instructions !== "string" || req.body.instructions.trim() === "" ||
       typeof req.body.step_number !== "number" || req.body.step_number < 1
